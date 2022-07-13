42c10b29e7fcf38cbc67beaeb1a7d5e1d3535de0
mastero <castro.alejandro17@gmail.com>
2022-07-13 14:31:59 -0500

    Primer Commit

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
+import { addTask } from "./components/addTask.js";
+import { displayTask } from "./components/readTask.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
+//ArroW functions o funciones anonimas
+btn.addEventListener("click", addTask);
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = ""
-    const content = ` <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
-
-    list.appendChild(task);
-
-    console.log(content);
-}
-
-
-console.log(btn);
-
-btn.addEventListener("click", createTask);
+displayTask();
